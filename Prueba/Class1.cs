273f5ae4c9c90fbf3a15d7519ec6a1116107110f
Dihota <diegotamayoctma@gmail.com>
2022-05-08 20:08:45 -0500

    Inicial

---
+++ Prueba/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace Prueba
+{
+    public class Class1
+    {
+
+        public string xmlread( string ruta)
+        {
+            string datos = string.Empty;
+            int Saltos = 0;
+            string texto = string.Empty;
+
+            XmlDocument readXML = new XmlDocument();
+            readXML.Load(ruta);
+
+            foreach (XmlNode N1 in readXML.DocumentElement.ChildNodes)
+            {
+
+                if (N1.Name == "cac:SenderParty")
+                {
+                    if (N1.HasChildNodes)
+                    {
+                        foreach (XmlNode N2 in N1.ChildNodes)
+                        {
+                            foreach (XmlNode N3 in N2.ChildNodes)
+                            {
+                                if (N3.Name == "cbc:RegistrationName" || N3.Name == "cbc:CompanyID")
+                                {
+                                    datos =datos +  N3.InnerText + ",";
+                                    Saltos++;
+                                }
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    if (N1.Name == "cbc:ParentDocumentID")
+                    {
+                        datos = datos + N1.InnerText + ",";
+                        Saltos++;
+                    }
+                }
+                
+                if (Saltos == 3)
+                {
+                    break;
+                }
+               
+            }
+
+            return datos;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Prueba;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 C1 = new Class1();
+            string dato =  C1.xmlread(@"C:\Users\57320\Documents\XML\SAPHETY_ad08001323020252200000425.xml");
+
+
+
+            string[] array = dato.Split('\u002C');
+
+            textBox1.Text = array[0];
+            textBox2.Text = array[1];
+            textBox3.Text = array[2];
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
